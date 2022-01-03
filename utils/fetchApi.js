c8f1211bc41dc394435fab349667570fb83f0ced
Anupam0202 <42176802+Anupam0202@users.noreply.github.com>
2022-01-04 01:49:54 +0530

    first commit

--- utils/fetchApi.js
+++ utils/fetchApi.js
@@ -6,7 +6,7 @@ export const fetchApi = async (url) => {
   const { data } = await axios.get((url), {
     headers: {
       'x-rapidapi-host': 'bayut.p.rapidapi.com',
-      'x-rapidapi-key': 'f9b009a5dfmsh64e503502281c06p1c9ce3jsna5a25d46cc20' ,
+      'x-rapidapi-key': 'f9b009a5dfmsh64e503502281c06p1c9ce3jsna5a25d46cc20'
     },
   });
     
